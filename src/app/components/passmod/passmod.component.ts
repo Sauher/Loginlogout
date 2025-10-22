6617b1c7b7c355ac50bc3122a0ceac04ea9e12c9
Sauher <schaueroliver@turr.hu>
2025-10-22 09:00:51 +0200

    AAAAAAAAAAAAAA

---
+++ src/app/components/passmod/passmod.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passmod',
+  standalone: true,
+  imports: [],
+  templateUrl: './passmod.component.html',
+  styleUrl: './passmod.component.scss'
+})
+export class PassmodComponent {
+
+}
